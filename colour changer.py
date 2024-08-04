87d32753010b89af428a3a906074a6dd7175aa1e
Yau Shing <75722100+yaushing@users.noreply.github.com>
2024-08-04 16:13:19 +0800

    FIX ALREADY

---
+++ colour changer.py
@@ -0,0 +1,18 @@
+import cv2
+import numpy as np
+FILE_PATH = "imgs/"
+for i in range(1, 66):
+    URL = "ezgif-frame-0{:02}".format(i) + ".png"
+    input = cv2.imread(FILE_PATH + URL, cv2.IMREAD_COLOR)
+    output = cv2.cvtColor(input, cv2.COLOR_BGR2RGBA)
+    col = (0, 0, 0)
+    tol = (25, 25, 25)
+    temp = np.subtract(input, col)
+    mask = (np.abs(temp) <= tol)
+    mask = (mask[:, :, 0] & mask[:, :, 1] & mask[:, :, 2])
+    temp[temp < 0] = 0                                            
+    alpha = (temp[:, :, 0] + temp[:, :, 1] + temp[:, :, 2]) / 3   
+    alpha[mask] = alpha[mask] / np.max(alpha[mask]) * 255         
+    alpha[~mask] = 255                                            
+    output[:, :, 3] = alpha
+    cv2.imwrite('output/' + "frame-{:02}".format(i) + ".png", output)
--- version.md
+++ version.md
@@ -1,5 +1,6 @@
 04 August 2024
-n
+- Added BTS for the splash images
+- Added cover splash images and code to remove the gif background, not the actual one yet
 - Added a license
 - Added version documentation and readme
 - Clean base plate, created navbar and home screen background
