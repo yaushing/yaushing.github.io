6dbefd890e1b08d8a0b9879e0af17990196a3641
Yau Shing <75722100+yaushing@users.noreply.github.com>
2024-08-07 16:15:45 +0800

    Fix

--- src/assets/home/images/arrow/sorter.py
+++
@@ -1,6 +0,0 @@
-import os
-real_count = 1
-PATH = "src/assets/home/images/arrow/"
-for i in range(27, 236):
-    os.rename(PATH + "{:03}.png".format(i), PATH + "{:03}.png".format(real_count))
-    real_count += 1
